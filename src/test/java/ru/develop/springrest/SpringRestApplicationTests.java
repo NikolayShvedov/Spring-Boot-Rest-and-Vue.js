b5815095196d7694b943fbc663745243f0997dcb
Николай Шведов <shved.nikolay.18@mail.ru>
2022-08-28 16:52:29 +0300

    add simple project

---
+++ src/test/java/ru/develop/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.develop.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
