2b3ef572926ddad2575fe408d8b2f178567aef91
Николай Шведов <shved.nikolay.18@mail.ru>
2022-09-09 13:46:51 +0300

    update abstract test class

--- src/test/java/ru/develop/springrest/SpringRestApplicationTests.java
+++ src/test/java/ru/develop/springrest/SpringRestApplicationTests.java
@@ -1,8 +1,18 @@
 package ru.develop.springrest;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.develop.springrest.initialize.Postgres;
 
-@SpringBootTest
-class SpringRestApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SpringRestApplication.class)
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = {
+        Postgres.Initializer.class
+})
+public abstract class SpringRestApplicationTests {
 
 }
